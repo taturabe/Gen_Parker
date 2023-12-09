455bdb297d15053cf9d2c51a03aa7574b08d25ff
taturabe <taturabe@amazon.co.jp>
2023-12-09 13:52:52 +0900

    deleted

--- test/env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#export MODEL_ID="arn:aws:bedrock:us-east-1:820974724107:provisioned-model/xu03nnpzk8gp"
-export MODEL_ID="arn:aws:bedrock:us-east-1:820974724107:provisioned-model/chx8tfmprdct"
-export MODEL_ID="arn:aws:bedrock:us-east-1:820974724107:provisioned-model/dglnjnsu7xwg"
-echo "MODEL_ID is set to: $MODEL_ID"
